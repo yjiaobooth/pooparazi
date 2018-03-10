d2a9faec82c76ef10d1710be650007f37b6333b3
Raghu Betina <raghu@firstdraft.com>
2018-03-10 03:01:57 +0000

    Only comment user can access show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :current_user_must_be_comment_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_comment_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_comment_user
     comment = Comment.find(params[:id])
